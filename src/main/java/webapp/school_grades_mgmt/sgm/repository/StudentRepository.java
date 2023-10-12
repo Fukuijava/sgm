cf41b3dea4652577f1e8ce573bce93c3315219ec
varo <sinigamiyossi@gmil.com>
2023-10-13 07:05:38 +0900

    ホーム画面の見た目変更

--- src/main/java/webapp/school_grades_mgmt/sgm/repository/StudentRepository.java
+++ src/main/java/webapp/school_grades_mgmt/sgm/repository/StudentRepository.java
@@ -4,5 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import webapp.school_grades_mgmt.sgm.entity.ClassEntity;
 import webapp.school_grades_mgmt.sgm.entity.StudentEntity;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<StudentEntity,Integer> {
+
+    List<StudentEntity> findByClassEntity(ClassEntity classEntity);
+
 }
