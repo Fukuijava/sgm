c1b60825ddb1866368d06e64d1804e586983f700
Fukuijava <fukujavai@gmail.com>
2023-10-31 10:58:21 +0900

    授業態度機能実装中

--- src/main/resources/static/js/classAttitude.js
+++ src/main/resources/static/js/classAttitude.js
@@ -1,25 +1,22 @@
 function changeSemester(){
-	var Semester = document.getElementById('semester');
-	var selectSemester = Semester.selectedIndex;
-	var semesterId = Semester.options[selectSemester].value;
-	var classId = document.getElementById('classId').value;
-	var studentCount = document.getElementById('studentCount').value;
-	var curriculumCount = document.getElementById('curriculumCount').value;
+    var Semester = document.getElementById('semester');
+    var selectSemester = Semester.selectedIndex;
+    var semesterId = Semester.options[selectSemester].value;
+    var classId = document.getElementById('classId').value;
+    var studentCount = document.getElementById('studentCount').value;
+    var curriculumCount = document.getElementById('curriculumCount').value;
     $.ajax({
-      type: "POST",
-      url: "/changeSemester",
-      data: { semesterId:semesterId, classId:classId},
+        type: "POST",
+        url: "/changeSemester",
+        data: { semesterId:semesterId, classId:classId},
     }).done(function( response ) {
-          $(".classCurriculum").empty();
-          var str = JSON.stringify(response);
-          for (i=1; i < studentCount; i++){
-            for(j=2; j < curriculumCount; j++){
-                listForm.rows[i].cells[j].innerText = response[0][1];
+        $(".classCurriculum").empty();
+        var arrayCount = 0;
+        for (i=1; i < studentCount+1; i++){
+            for(j=2, k=0; k < curriculumCount; j++, k++, arrayCount++){
+            listForm.rows[i].cells[j].innerHTML =
+                '<input type="number"  min="1" max="5" value="'+ response[arrayCount][1] +'"/>';
             }
-          }
-
-      });
-  }
-
-//listForm.rows[i].cells[j].innerHTML = response[0][1];
-//     <input type="number"  min="1" max="5" th:value="${classAttitude[__${studentCount.index}__].classAttitudeEvaluation}"/>
+        }
+    });
+}
